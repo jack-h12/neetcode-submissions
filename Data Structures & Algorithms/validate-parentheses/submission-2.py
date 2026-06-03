1d2c30a5b93f1f9541a77ce48d97868ea746a0f1
Jack Harken <160553400+jack-h12@users.noreply.github.com>
2026-06-03 17:59:44 +1200

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+        for c in s:
+            if c == "(" or c == "{" or c == "[":
+                stack.append(c)
+            else:
+                if len(stack) == 0:
+                    return False
+                elif c == ")":
+                    if stack[-1] != "(":
+                        return False
+                    else:
+                        stack.pop()
+                elif c == "}":
+                    if stack[-1] != "{":
+                        return False
+                    else:
+                        stack.pop()
+                elif c == "]":
+                    if stack[-1] != "[":
+                        return False
+                    else:
+                        stack.pop()
+        if len(stack) != 0:
+            return False
+        return True
