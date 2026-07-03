c65ba30e0965044d4015b8d7bb983ea50b50faf7
Jack Harken <160553400+jack-h12@users.noreply.github.com>
2026-07-03 22:17:24 +1200

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        s1_char_frequency = [0] * 26
+        for c in s1:
+            s1_char_frequency[ord(c) - ord('a')] += 1
+
+        l, r = 0, len(s1) - 1
+        for r in range(r, len(s2)):
+            print("current s2 substring is ", s2[l:r+1])
+            s2_char_frequency = [0] * 26
+            for c in s2[l:r+1]:
+                s2_char_frequency[ord(c) - ord('a')] += 1
+            if s2_char_frequency == s1_char_frequency:
+                return True
+            l += 1
+        return False
